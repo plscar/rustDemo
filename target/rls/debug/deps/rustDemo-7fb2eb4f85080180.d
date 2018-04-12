387dc47342fd4e1da2828e3caf1822833770487f
Felix <452613808@qq.com>
2018-04-12 14:38:06 +0800

    再次更改md文件格式

---
+++ target/rls/debug/deps/rustDemo-7fb2eb4f85080180.d
@@ -0,0 +1,7 @@
+/home/felix/文档/rust/rustDemo/target/rls/debug/deps/rustDemo-7fb2eb4f85080180.rmeta: src/lib.rs src/about_trait/mod.rs src/about_trait/mytrait.rs
+
+/home/felix/文档/rust/rustDemo/target/rls/debug/deps/rustDemo-7fb2eb4f85080180.d: src/lib.rs src/about_trait/mod.rs src/about_trait/mytrait.rs
+
+src/lib.rs:
+src/about_trait/mod.rs:
+src/about_trait/mytrait.rs:
---
+++ target/rls/debug/deps/rustDemo-f8829c7c4b338dad.d
@@ -0,0 +1,5 @@
+/home/felix/文档/rust/rustDemo/target/rls/debug/deps/rustDemo-f8829c7c4b338dad.rmeta: src/main.rs
+
+/home/felix/文档/rust/rustDemo/target/rls/debug/deps/rustDemo-f8829c7c4b338dad.d: src/main.rs
+
+src/main.rs:
